d34f8b8a2f125b90cc9aecaa6a6d7b4d9351031a
Karen Ruiz <anakaren.ruizb@gmail.com>
2020-10-16 13:21:25 -0500

    estructura para contacto

--- contacto.php
+++ contacto.php
@@ -1,3 +1,69 @@
-<?php include_once 'assets\header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Joaquin Mendoza ">
 
-<?php include_once 'assets\footer.php'; ?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="icon" href="images/dieta.png">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets\images\diet.ico" type="image/x-icon">
+    <title>Contacto</title>
+</head>
+<body>
+    <!-- header -->
+     <?php include_once 'assets\header.php'; ?>
+    <!-- /.. -->
+
+    <!-- main -->
+    <main>
+        <section>
+        <div class="card mb-3" style="min-width: 540px; min-height: 300px">
+        <div class="col-md-10">
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Example select</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect2">Example multiple select</label>
+                    <select multiple class="form-control" id="exampleFormControlSelect2">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+            </div>
+        </div>
+            
+        </section>
+    </main>
+    <!-- /.. -->
+
+    <!-- footer -->
+    <?php include_once 'assets\footer.php'; ?>
+    <!-- /.. -->
+</body>
+</html>
