a1d56be14df354c9a5aaa419804a98b4a4c3f2d3
Hugo Lara <hugo.alex200197@outlook.com>
2020-10-16 13:39:06 -0500

    Sin comentario

--- index.php
+++ index.php
@@ -1,54 +1,13 @@
 <?include 'assets/header.php';?>
-
-    <!--section id="slider">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="assets/images/MON1.png" alt="First slide">
-                    <div class="carousel-caption">
-                        <h3 class="d-block">Imagen de 2016 en la grabacion del video de "Amor completo</h3>
-                        <p class="lead d-none d-sm-block">AlbuM "Mon laferte V1</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="assets/images/mon2.jpg" alt="Second slide">
-                    <div class="carousel-caption">
-                        <h3 class="d-block">Imagen en vivo del concierto en chile</h3>
-                        <p class="lead d-none d-sm-block">Viña del mar</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="assets/images/mon3.jpg" alt="Second slide">
-                    <div class="carousel-caption">
-                        <h3 class="d-block">Sesion de fotos</h3>
-                        <p class="lead d-none d-sm-block">Mexico.</p>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previa</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Siguiente</span>
-            </a>
-        </div>
-    </!--section-->
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <h2 class="text-light">Albums mas escuchados de Mon Laferte</h2>
-                    <p class="lead text-muted">En este apartado se podra presenciar los albunes mas escuchados de la interprete chilena.</p>
+                    <h2 class="text-light">Bienvenido</h2>
+                    <p class="lead text-muted">Baja de peso/Aumento de masa muscular. Lleva una vida más saludable</p>
                     <div class="portfolio-item mt-6">
                         <a href="#">
-                            <img src="assets/images/vol1.jpg" class="img-fluid">
+                            <img src="" class="img-fluid">
                             <div class="portfolio-details bg-light p-4">
                                 <h4 class="text-dark">Mon laferte V1</h4>
                                 <p class="text-muted mb-0">La edición especial del álbum, viene acompañado con un DVD que contiene la grabación del conciento de Mon Laferte en el Lunario del Auditorio Nacional, ocurrido el 16 de octubre de 2015</p>
@@ -57,7 +16,7 @@
                     </div>
                     <div class="portfolio-item mt-6 mb-6">
                         <a href="#">
-                            <img src="assets/images/vol2.jpg" class="img-fluid">
+                            <img src="" class="img-fluid">
                             <div class="portfolio-details bg-light p-4">
                                 <h4 class="text-dark">La trenza</h4>
                                 <p class="text-muted mb-0">La trenza es el quinto álbum de estudio de la cantante chilena Mon Laferte, publicado el 28 de abril de 2017</p>
@@ -68,7 +27,7 @@
                 <div class="col-md-6">
                     <div class="portfolio-item">
                         <a href="#">
-                            <img src="assets/images/vol3.jpg" class="img-fluid">
+                            <img src="" class="img-fluid">
                             <div class="portfolio-details bg-light p-4">
                                 <h4 class="text-dark">Norma</h4>
                                 <p class="text-muted mb-0">Norma es el sexto álbum de estudio de Mon Laferte, ​ publicado el 9 de noviembre de 2018.​  </p>
@@ -77,7 +36,7 @@
                     </div>
                     <div class="portfolio-item mt-6">
                         <a href="#">
-                            <img src="assets/images/vol4.jpg" class="img-fluid">
+                            <img src="" class="img-fluid">
                             <div class="portfolio-details bg-light p-4">
                                 <h4 class="text-dark">Sola con mis monstruos </h4>
                                 <p class="text-muted mb-0">Sola con mis monstruos es el primer Álbum en vivo de la cantautora chilena Mon Laferte, donde presenta 18 canciones en formato acústico, solo acompañada de su guitarrista Sebastián Aracena, grabados de sus dos presentaciones realizadas el 12 de octubre de 2019 en el Lunario del Auditorio Nacional de México y publicado el 31 de enero de 2020.5​</p>
