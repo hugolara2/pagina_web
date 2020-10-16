71c0093472fc9cd9d255d5f7220d41667603849c
Hugo Lara <hugo.alex200197@outlook.com>
2020-10-16 13:44:03 -0500

    Cambios aceptados

--- index.php
+++ index.php
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-<?include 'assets/header.php';?>
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2 class="text-light">Bienvenido</h2>
-                    <p class="lead text-muted">Baja de peso/Aumento de masa muscular. Lleva una vida más saludable</p>
-                    <div class="portfolio-item mt-6">
-                        <a href="#">
-                            <img src="" class="img-fluid">
-                            <div class="portfolio-details bg-light p-4">
-                                <h4 class="text-dark">Mon laferte V1</h4>
-                                <p class="text-muted mb-0">La edición especial del álbum, viene acompañado con un DVD que contiene la grabación del conciento de Mon Laferte en el Lunario del Auditorio Nacional, ocurrido el 16 de octubre de 2015</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="portfolio-item mt-6 mb-6">
-                        <a href="#">
-                            <img src="" class="img-fluid">
-                            <div class="portfolio-details bg-light p-4">
-                                <h4 class="text-dark">La trenza</h4>
-                                <p class="text-muted mb-0">La trenza es el quinto álbum de estudio de la cantante chilena Mon Laferte, publicado el 28 de abril de 2017</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="portfolio-item">
-                        <a href="#">
-                            <img src="" class="img-fluid">
-                            <div class="portfolio-details bg-light p-4">
-                                <h4 class="text-dark">Norma</h4>
-                                <p class="text-muted mb-0">Norma es el sexto álbum de estudio de Mon Laferte, ​ publicado el 9 de noviembre de 2018.​  </p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="portfolio-item mt-6">
-                        <a href="#">
-                            <img src="" class="img-fluid">
-                            <div class="portfolio-details bg-light p-4">
-                                <h4 class="text-dark">Sola con mis monstruos </h4>
-                                <p class="text-muted mb-0">Sola con mis monstruos es el primer Álbum en vivo de la cantautora chilena Mon Laferte, donde presenta 18 canciones en formato acústico, solo acompañada de su guitarrista Sebastián Aracena, grabados de sus dos presentaciones realizadas el 12 de octubre de 2019 en el Lunario del Auditorio Nacional de México y publicado el 31 de enero de 2020.5​</p>
-                            </div>
-                        </a>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -101,7 +55,6 @@
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <a href="#" class="btn btn-primary"><img src="assets\images\plus_icon.ico" alt="" width="20" height="20" title="ver más"></a>
                     </div>
->>>>>>> ce880ba46f9d6d160d1bdc96ce44314063ac8d0e
                     </div>
                 </div>
                 <div class="col mb-6">
